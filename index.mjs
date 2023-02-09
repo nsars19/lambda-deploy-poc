2cc273acbda61ece37423db98af515b0a8635291
Nick Sardonini <nsardonini@umassp.edu>
2023-02-09 15:05:23 -0500

    add axios call to test data fetching

--- index.mjs
+++ index.mjs
@@ -1,8 +1,21 @@
+import axios from "axios";
+
 export const handler = async (event) => {
-  const data = (String(event.data) || "Here's some data!").toUpperCase();
-  const response = {
-    statusCode: 200,
-    body: { data },
-  };
-  return response;
+  try {
+    const data = (String(event.data) || "Here's some data!").toUpperCase();
+    const axiosRes = await axios.get("https://example.com");
+    const axiosData = axiosRes.data;
+    const response = {
+      statusCode: 200,
+      body: { data: axiosData },
+    };
+    return response;
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: {
+        data: "OOPS something broke!",
+      },
+    };
+  }
 };
