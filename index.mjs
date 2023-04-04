080459fed255eca1db03099e63457e97a87d6eb7
Nick Sardonini <nsardonini@umassp.edu>
2023-04-04 17:11:30 -0400

    change api

--- index.mjs
+++ index.mjs
@@ -3,8 +3,15 @@ import axios from "axios";
 export const handler = async (event) => {
   try {
     const data = (String(event.data) || "Here's some data!").toUpperCase();
-    const axiosRes = await axios.get("https://catfact.ninja/fact");
-    const axiosData = axiosRes.data.fact;
+    // const axiosRes = await axios.get("https://catfact.ninja/fact");
+    // const axiosData = axiosRes.data.fact;
+
+    const axiosRes = await axios.get(
+      "https://official-joke-api.appspot.com/random_joke"
+    );
+    const { setup, punchline } = axiosRes.data;
+    const axiosData = `${setup}: ${punchline}`;
+
     const response = {
       statusCode: 200,
       body: { data: axiosData },
