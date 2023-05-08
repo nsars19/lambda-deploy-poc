fd7c6baca4def7d4964beb49290ba0fd4c40a61e
Nick Sardonini <nsardonini@umassp.edu>
2023-05-08 15:27:33 -0400

    switch to cat fact api

--- index.mjs
+++ index.mjs
@@ -3,14 +3,14 @@ import axios from "axios";
 export const handler = async (event) => {
   try {
     const data = (String(event.data) || "Here's some data!").toUpperCase();
-    // const axiosRes = await axios.get("https://catfact.ninja/fact");
-    // const axiosData = axiosRes.data.fact;
+    const axiosRes = await axios.get("https://catfact.ninja/fact");
+    const axiosData = axiosRes.data.fact;
 
-    const axiosRes = await axios.get(
-      "https://official-joke-api.appspot.com/random_joke"
-    );
-    const { setup, punchline } = axiosRes.data;
-    const axiosData = `${setup} ${punchline}`;
+    // const axiosRes = await axios.get(
+    //   "https://official-joke-api.appspot.com/random_joke"
+    // );
+    // const { setup, punchline } = axiosRes.data;
+    // const axiosData = `${setup} ${punchline}`;
 
     const response = {
       statusCode: 200,
